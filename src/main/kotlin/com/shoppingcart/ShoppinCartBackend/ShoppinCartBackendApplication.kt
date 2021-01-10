1758b560351b7f06f493e938629d70cbfcad25c2
ragnarok <juankcontreras00@gmail.com>
2021-01-10 00:05:14 -0500

    update

--- src/main/kotlin/com/shoppingcart/ShoppinCartBackend/ShoppinCartBackendApplication.kt
+++ src/main/kotlin/com/shoppingcart/ShoppinCartBackend/ShoppinCartBackendApplication.kt
@@ -1,10 +1,24 @@
 package com.shoppingcart.ShoppinCartBackend
 
+import com.shoppingcart.ShoppinCartBackend.dao.ProductRepository
+import com.shoppingcart.ShoppinCartBackend.model.Product
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ShoppinCartBackendApplication
+class ShoppinCartBackendApplication: CommandLineRunner {
+
+	@Autowired
+	val productRepository: ProductRepository? = null
+	override fun run(vararg args: String?) {
+		val product1 = Product("laptop", 123, "Asus Core i7")
+
+		productRepository!!.save(product1)
+	}
+
+}
 
 fun main(args: Array<String>) {
 	runApplication<ShoppinCartBackendApplication>(*args)
